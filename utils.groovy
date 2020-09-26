2364cf8344e110b8a305f45175cc338211c0e169
sathyadevops476 <sathyadevops476@gmail.com>
2020-09-26 19:00:27 +0530

    adding a simple list in groovy file

--- utils.groovy
+++ utils.groovy
@@ -1,4 +1,7 @@
 def version="1.0" 
+def list_processor = ${params.SIMPLE_LIST}.split(',')
+
+
 
 def buildApp(){
 	echo "Building the application ${version}" 
@@ -9,6 +12,7 @@ def compileApp(){
 }
 def testApp(){
 	echo "Testing the application"
+	echo "List Processor------> ${list_processor[1]}"
 }
 
 def deployApp(){
