40774ee13e7e8f9e6a1a4c7e05683cd4df4410ce
sathyadevops476 <sathyadevops476@gmail.com>
2020-09-27 13:35:21 +0530

    added temp groovy script1

--- temp.groovy
+++ temp.groovy
@@ -5,11 +5,9 @@ def jobName = "multibranchtest"
 def job = jenkins.getItem(jobName)
 
 def doGetAllJenkinsDetails(){
-	println "Job type: ${job.getClass()}"
-	println "Is building: ${job.isBuilding()}"
-	println "Is in queue: ${job.isInQueue()}"
-	println "Last successfull build: ${job.getLastSuccessfulBuild()}"
-	println "Last failed build: ${job.getLastFailedBuild()}"
-	println "Last build: ${job.getLastBuild()}"
-	println "All builds: ${job.getBuilds().collect{ it.getNumber()}}"
+	def lastSuccessfulPipelineID = build.getProject().getLastSuccessfulBuild().properties.get("envVars")['PipelineNumber']
+	def pa = new ParametersAction([
+	  new StringParameterValue("lastSuccessfulPipelineID", lastSuccessfulPipelineID)
+	])
+	println(lastSuccessfulPipelineID)
 }
