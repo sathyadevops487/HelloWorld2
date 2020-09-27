ce72e7d5dad7fb6f38eef53a79476e9aa649673f
sathyadevops476 <sathyadevops476@gmail.com>
2020-09-27 16:12:53 +0530

    added request property3

--- Http.groovy
+++ Http.groovy
@@ -1,17 +1,9 @@
 
 def doSimpleHttp(){
-		 def baseUrl = new URL('http://localhost:8080/job/multibranchpipeline/job/master/lastSuccessfulBuild/api/json/buildNumber')
-		 
-		 
-		 String authHeaderValue = "Basic " + new String(auth); 
-		 BASE64Encoder enc = new sun.misc.BASE64Encoder();
-		 String auth = "admin" + ":" + "f730b47332fd4368908c92e230afc82b";
-		 String encodedAuthorization = enc.encode( auth.getBytes() );
-		 
+		 def baseUrl = new URL('http://ergast.com/api/f1/2004/1/results.json')
+		
 		 HttpURLConnection connection = (HttpURLConnection) baseUrl.openConnection();
-		 connection.addRequestProperty("Accept", "application/json");
-		 connection.setRequestProperty("Authorization", "Basic "+
-			 encodedAuthorization);
+		 connection.addRequestProperty("Accept", "application/json");	 
 		 
 		 connection.with {
 		   doOutput = true
