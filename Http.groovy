d6c483ff8242a6ae53dfbea85749fec4a8e5c370
sathyadevops476 <sathyadevops476@gmail.com>
2020-09-27 16:34:05 +0530

    added request property5

--- Http.groovy
+++ Http.groovy
@@ -2,19 +2,18 @@ import groovy.json.JsonSlurper;
 
 def doSimpleHttp(){
 		 def baseUrl = new URL('http://ergast.com/api/f1/2004/1/results.json')
-		
-		 HttpURLConnection connection = (HttpURLConnection) baseUrl.openConnection();
-		 connection.addRequestProperty("Accept", "application/json");	 
+		 try{
+			 HttpURLConnection connection = (HttpURLConnection) baseUrl.openConnection();
+			 connection.addRequestProperty("Accept", "application/json");
+			 
+			 if(connection.responseCode == 200){
+					def json =  new JsonSlurper().parse(connection.inputStream.text)
+					println json.query.MRData.url
+			 }
+		 }catch(Exception ex){
+			 println "Error!!"
+		 }
 		 
-		 connection.with {
-		   doOutput = true
-		   requestMethod = 'GET'
-		   println connection.inputStream.text
-		 }
-		 if(connection.responseCode == 200){
-				def json =  new JsonSlurper().parse(connection.inputStream.text)
-				println json.query.MRData.url
-		 }
 }
 
 return this
