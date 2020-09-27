109b1ef5962813d0fdec68360e875e992b29da2d
sathyadevops476 <sathyadevops476@gmail.com>
2020-09-27 16:03:09 +0530

    added request property1

--- Http.groovy
+++ Http.groovy
@@ -2,8 +2,8 @@
 def doSimpleHttp(){
 		 def baseUrl = new URL('http://localhost:8080/job/multibranchpipeline/job/master/lastSuccessfulBuild/api/json/buildNumber')
 		 String auth = "admin" + ":" + "f730b47332fd4368908c92e230afc82b";
-		 byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(StandardCharsets.UTF_8));
-		 String authHeaderValue = "Basic " + new String(encodedAuth); 
+		 
+		 String authHeaderValue = "Basic " + new String(auth); 
 		 
 		 HttpURLConnection connection = (HttpURLConnection) baseUrl.openConnection();
 		 connection.addRequestProperty("Accept", "application/json")
