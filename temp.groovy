6000b69cfd6e0b7fc749e0daad5d5b666f813942
sathyadevops476 <sathyadevops476@gmail.com>
2020-09-27 15:29:45 +0530

    added list number4

--- temp.groovy
+++ temp.groovy
@@ -4,7 +4,7 @@ import jenkins.*
 
 def doGetAllJenkinsDetails(jobname){
 	def list = ["Kay","Henry","Justin","Tom"]
-	def lastSuccessfulPipelineID = Jenkins.instance.getItem("multibranchpipeline").lastCompletedBuild.displayname
+	def lastSuccessfulPipelineID = Jenkins.instance.getItem("multibranchpipeline").lastSuccessfulBuild.displayName
 	for (int f = 0; f < list.size(); f++){
 		echo "${list[f]}"
 	}
