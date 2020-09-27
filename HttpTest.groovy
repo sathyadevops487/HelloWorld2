5e25960bc41f3941f22e124bcc8f2551c077f77d
sathyadevops476 <sathyadevops476@gmail.com>
2020-09-27 18:26:15 +0530

    updated http methods

--- HttpTest.groovy
+++ HttpTest.groovy
@@ -1,19 +1,19 @@
-import java.net.URL
-import groovy.json.JsonSlurper
+import java.net.URL;
+import groovy.json.JsonSlurper;
 
 def testHttpMethod(){
 	def baseUrl = new URL('http://ergast.com/api/f1/2004/1/results.json')
 		
 	HttpURLConnection connection = (HttpURLConnection)baseUrl.openConnection();
-	connection.addRequestProperty("Accept", "application/json")
+	connection.addRequestProperty("Accept", "application/json");
 	
 	/*connection.with{
 		doOutput = true
 		requestMethod = 'GET'
 		println connection.inputStream.text
 	}*/
-	if(connection.responseCode == 200){
-		def json = new JsonSlurper.parse(connection.inputStream)
+	if(connection.responseCode == 200){		
+		def json = new JsonSlurper.parse(connection.inputStream);
 		//def json = new JsonSlurper.parseText(connection.inputStream.text)
 		print "URL from API ------> "+json.MRData.url
 	}
