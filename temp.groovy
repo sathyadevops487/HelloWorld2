a27207b2e8d5326d26a296ab6cc94146b96a5aed
sathyadevops476 <sathyadevops476@gmail.com>
2020-09-27 14:05:38 +0530

    added temp groovy script8

--- temp.groovy
+++ temp.groovy
@@ -3,9 +3,7 @@ import jenkins.model.*
 def doGetAllJenkinsDetails(){
 	def jenkins = Jenkins.instance.getItem("multibranchtest")
 	
-	println "Job type: ${jenkins.getClass()}"
-	println "Is building: ${jenkins.isBuilding()}"
-	println "Is in queue: ${jenkins.isInQueue()}"
+	println "Job type: ${jenkins.getClass()}"	
 	println "Last successfull build: ${jenkins.getLastSuccessfulBuild()}"
 	println "Last failed build: ${jenkins.getLastFailedBuild()}"
 	println "Last build: ${jenkins.getLastBuild()}"
