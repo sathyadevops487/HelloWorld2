3cea28552736da907a7bd1b9674b2d3da0a53aad
sathyadevops476 <sathyadevops476@gmail.com>
2020-09-27 17:28:43 +0530

    updated datatypes with map

--- datatypes.groovy
+++ datatypes.groovy
@@ -1,8 +1,15 @@
-def testListMethod(){
-	
+def testListMethod(){	
 	def list = ["apples", "mangoes", "oranges","pappaya"];
 	for(int f=0; f<=list.size()-1;f++){
 		println "${list[f]}"
+	}
+}
+
+def testMapMethod(){
+	def m_map = [name:"Jerry", age:42, city:"Bangalore"]
+	print m_map.get("city")
+	m_map.each{entry->
+		println "$entry.key", "$entry.value"
 	}
 }
 return this
