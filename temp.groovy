23ef68e33c65b22526fea7b38907d0536c6ae447
sathyadevops476 <sathyadevops476@gmail.com>
2020-09-27 14:37:17 +0530

    added temp groovy script104

--- temp.groovy
+++ temp.groovy
@@ -3,7 +3,7 @@ import jenkins.*
 
 
 def doGetAllJenkinsDetails(jobname){
-	def lastSuccessfulPipelineID = Jenkins.instance.getItem(jobname).lastSuccessfulBuild.displayName
+	def lastSuccessfulPipelineID = Jenkins.instance.getItem("multibranchpipeline").lastSuccessfulBuild.displayName
 	def pa = new ParametersAction([
 		new StringParameterValue("lastSuccessfulPipelineID", lastSuccessfulPipelineID)
 	])
